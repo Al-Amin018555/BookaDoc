cfa8ff551116d4851cdac69b47266a227f15923f
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-10-07 21:22:25 +0600

    project started with tailwind daisyUI react router installed

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
